d24d89095cf0e90b79090fb75d1f6a8a5c5437d3
Michael Myung <michael.h.myung@gmail.com>
2017-12-13 23:11:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution in this file!
+function logDriverNames (drivers){
+  function logName (drivers){
+    console.log(drivers.name)
+  }
+  return drivers.forEach(logName)
+}
+
+
+//logDriversByHometown()
+//Receives an array of driver objects as the first argument
+//and a location as the second argument.
+//The function logs to the console the name attribute
+//of each driver whose hometown matches the string passed in as the 'location' argument.
+
+function logDriversByHometown (drivers, location){
+  function callBack(element, index, array){
+    if (location === element.hometown){
+      console.log(element.name)
+    }
+  }
+  return drivers.forEach(callBack)
+}
+
+
+function driversByRevenue (drivers){
+  const numberSorter = function (num1, num2) {
+    return num1.revenue - num2.revenue
+  }
+  const sortedDrivers = drivers.slice()
+  return sortedDrivers.sort(numberSorter)
+}
+
+function driversByName (drivers){
+  const nameSorter = function (a,b){
+    return a.name.localeCompare(b.name)
+  }
+  const sortedDrivers = drivers.slice()
+  return sortedDrivers.sort(nameSorter)
+}
+
+function totalRevenue (drivers){
+  const addToTotal = function(agg, el, i, arr){
+    return agg + el.revenue
+  }
+  return drivers.reduce(addToTotal, 0)
+}
+
+function averageRevenue (drivers){
+  const avgRev = function(agg, el, i, arr){
+    return agg + el.revenue / arr.length
+  }
+  return drivers.reduce(avgRev, 0)
+}
